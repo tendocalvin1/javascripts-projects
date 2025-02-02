88258203df1900762bb25cfd425f54f1b624ddc4
tendocalvin1 <125687687+tendocalvin1@users.noreply.github.com>
2025-02-02 18:21:09 +0300

    Add files via upload

--- JAVASCRIPT WORK/basics2.js
+++ JAVASCRIPT WORK/basics2.js
@@ -78,5 +78,72 @@ greeting('Donald Trump!');
 
 /* when creating functions in javascript, we use the "FUNCTION" keyword */
 
+function greet(name){
+    console.log("Hello " + name);
+}
+greet("Alice!")
+
+function calculateSum(numbers){
+   let sum = 0;
+   for (let i=0; i < numbers.length; i++){
+    sum += numbers[i];
+   }
+   return sum;
+}
+
+const result = calculateSum([1,2,3,4])
+console.log("The total sum of the numbers:", result)
+
+function sum(numbers){
+    for(let i = 0;i < numbers.length; i++);{
+    sum += numbers[i];
+    }
+    return sum;
+}
+
+const addition = calculateSum([3,4,5,6])
+console.log("Total sum:",addition)
 
 
+function findmax(numbers){
+    for(let i =0; i < numbers.length;i++);{
+    findmax = numbers[1];
+    }
+    return findmax;
+    
+}
+
+const max_number = findmax([3,9,2,7,1])
+console.log("The maximum number is:",max_number)
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+const factor = factorial(5);
+console.log("The factorial of 5 is:", factor); // The factorial of 5 is: 120
+
+// Types of functions in Javascript //
+function multiply(num1, num2){
+    return num1 + num2;
+}
+console.log(multiply(2,2) );
+
+// what are the two reasons for creating functions in Javascript ?//
+// 1. To perform an action 
+// 2. To calculate a value and return that value.
+
+// Functions in JavaScript are essential for several reasons, but here are two key ones:
+// 1. Reusability: Functions allow you to write a piece of code once and then reuse it whenever needed. This helps reduce redundancy and makes the code more modular and maintainable. For example, if you have a function that calculates the sum of two numbers, you can use this function multiple times without rewriting the same logic.
+// 2.Readability and Organization: By using functions, you can break down complex problems into smaller, more manageable chunks. This makes your code easier to read, understand, and debug. Functions provide a clear structure to your code and help in organizing it logically. This way, you can focus on individual tasks or components separately.
+
+
+// Introduction to operators.//
+// assignment operators //
+let programmingLanguange = 'javascript';
+console.log(programmingLanguange);
+
+ 
---
+++ JAVASCRIPT WORK/control_flows.js
@@ -0,0 +1,6 @@
+// comparison operators.//
+let number1 = 14;
+let number2 = 10;
+
+const isnumber1GreaterOrEqualTo = number1 >= number2; 
+console.log( isnumber1GreaterOrEqualTo);
