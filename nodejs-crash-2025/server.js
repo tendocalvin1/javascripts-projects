e0d994fb73f420b79c3147f4e1c403f4b02a34c6
tendocalvin1 <125687687+tendocalvin1@users.noreply.github.com>
2025-03-01 08:32:58 +0300

    Add files via upload

---
+++ nodejs-crash-2025/server.js
@@ -0,0 +1,8 @@
+import { getPosts } from './postcontroller.js';
+console.log(getPosts());
+
+
+
+// const  {generateRandomnumber, celsiusToFahrenheit} = require('./utils');
+// console.log("Random Number:", generateRandomnumber());
+// console.log(`celsius to fahrenheit: ${celsiusToFahrenheit(0)}`);
